a69183c14c9857bfc3b828ea05591d25c3b6cb79
Diana Mae <dianamae@example.com>
2023-06-22 13:19:31 +0800

    Display Genres

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -6,7 +6,7 @@ import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
      const {data, error, isloading} = useGames()
-     const skeletons = [1,2,3,3,4,5,6];
+     const skeletons = [1,2,3,3,4,5,6,7,8,9,10,11,12,13,14];
   return (
     <>
         {error && <Text>{error}</Text>}
--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,12 +1,20 @@
+import { Text, HStack, List, ListItem, Image } from '@chakra-ui/react';
 import useGenres, { Genre } from '../hooks/useGenres';
+import CropImageURL from '../Services/image-url';
 
 const GenreList = () => {
     const { data} = useGenres();
    
     return (
-    <ul>
-      {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
-    </ul>
+    <List>
+      {data.map(genre => 
+      <ListItem key={genre.id} paddingY="5px">
+        <HStack>
+          <Image src={CropImageURL(genre.image_background)} boxSize="32px" borderRadius={8}/>
+          <Text fontSize="lg">{genre.name}</Text>
+        </HStack>
+      </ListItem>)}
+    </List>
   )
 }
 
