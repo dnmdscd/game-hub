2ba28fe1423e0d882a4cd8d3a5219ec19e2b5a07
Diana Mae <dianamae@example.com>
2023-07-06 08:48:11 +0800

    Ship platforms with the app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,9 +1,11 @@
 import genres from "../data/genres";
+
 export interface Genre {
-    id: number;
-    name: string;
-    image_background: string
+  id: number;
+  name: string;
+  image_background: string;
 }
-  
-const useGenres = () =>({ data:genres, isloading: false, error: null});
+
+const useGenres = () => ({ data: genres, isLoading: false, error: null })
+
 export default useGenres;
