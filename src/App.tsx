5be44f6c9873e8033d2c30a89f893f79d109ab9e
Diana Mae <dianamae@example.com>
2023-07-06 10:53:37 +0800

    Refactor: Simplify game query

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./Components/NavBar";
 import GameGrid from "./Components/GameGrid";
 import GenreList from "./Components/GenreList";
 import { useState } from "react";
-import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./Components/PlatfromSelector";
 import SortSelector from "./Components/SortSelector";
 import GameHeading from "./Components/GameHeading";
-import { Platform } from "./hooks/usePlatforms";
 
 export interface GameQuery{
-  genre: Genre | null;
-  platform: Platform | null;
+  genreId?: number;
+  platformId?: number;
   sortOrder: string;
   searchText: string;
 }
@@ -32,35 +30,23 @@ function App() {
    <GridItem area="nav">
       <NavBar onSearch={(searchText) => setGameQuery({...gameQuery, searchText})}/>
     </GridItem> 
-
     <Show above="lg">
       <GridItem area="aside" paddingX={5}>
-        <GenreList selectedGenre={gameQuery.genre} onSelectGenre={(genre)=> setGameQuery({...gameQuery, genre })}/>
+        <GenreList selectedGenreId={gameQuery.genreId} onSelectGenre={(genre)=> setGameQuery({...gameQuery, genreId: genre.id })}/>
       </GridItem>
     </Show>
-
-    <GridItem area="main">
-      <Box  paddingLeft={2}>
-
-        <GameHeading gameQuery={gameQuery}/>
-
-        <Flex marginBottom={5}>
-          
-          <Box marginRight={5}>
-            <PlatformSelector selectedPlatform= {gameQuery.platform} onSelectedPlatform={(platform) => setGameQuery({...gameQuery, platform})} />
-          </Box>
-
-          
-          <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder)=> setGameQuery({...gameQuery, sortOrder})} />
-
-
-        </Flex>
-      </Box>
-      <GameGrid gameQuery={gameQuery}/>
-    </GridItem>
-
-
-
+      <GridItem area="main">
+        <Box  paddingLeft={2}>
+          <GameHeading gameQuery={gameQuery}/>
+          <Flex marginBottom={5}>   
+            <Box marginRight={5}>
+              <PlatformSelector selectedPlatformId= {gameQuery.platformId} onSelectedPlatform={(platform) => setGameQuery({...gameQuery, platformId: platform.id})} />
+            </Box>
+            <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder)=> setGameQuery({...gameQuery, sortOrder})} />
+          </Flex>
+        </Box>
+        <GameGrid gameQuery={gameQuery}/>
+      </GridItem>
   </Grid>
 }
 
--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -4,10 +4,10 @@ import CropImageURL from '../Services/image-url';
 
 interface Props{
   onSelectGenre: (genre: Genre)=> void;
-  selectedGenre: Genre | null;
+  selectedGenreId?: number;
 }
 
-const GenreList = ({selectedGenre,onSelectGenre}: Props) => {
+const GenreList = ({selectedGenreId, onSelectGenre}: Props) => {
     const { data, isLoading, error} = useGenres();
 
     if (error) return null;
@@ -21,7 +21,7 @@ const GenreList = ({selectedGenre,onSelectGenre}: Props) => {
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
             <Image src={CropImageURL(genre.image_background)} boxSize="32px" borderRadius={8} objectFit="cover"/>
-            <Button whiteSpace="normal" textAlign="left" fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}  onClick={()=> onSelectGenre(genre)} fontSize="lg" variant="link">{genre.name}</Button>
+            <Button whiteSpace="normal" textAlign="left" fontWeight={genre.id === selectedGenreId ? 'bold' : 'normal'}  onClick={()=> onSelectGenre(genre)} fontSize="lg" variant="link">{genre.name}</Button>
           </HStack>
         </ListItem>)}
       </List>
